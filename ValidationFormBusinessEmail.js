35fbc4b7729a81f03fcca3740a3e62c7ef5c252f
Philipe Coutinho <140984753+coutinhophilipe@users.noreply.github.com>
2023-09-06 09:53:06 -0300

    Create ValidationFormBusinessEmail.js

---
+++ ValidationFormBusinessEmail.js
@@ -0,0 +1,34 @@
+function Validacao(email) {
+
+    // Variável dentro da função para que o valor seja atualizado toda chamada nova da função 
+    //let email = txtemail.value;
+
+    // Regex para verificar se o e-mail tem um "@" e pelo menos um "." após o "@"
+    let FormatoValido = /@.*\./;
+
+    // Regex para verificar se o e-mail tem um ","
+    let FormatoInvalido = /,/;
+    
+    // Regex para excluir alguns domínios de e-mail comuns
+    let DominiosProibidos = /@(gmail\.com|yahoo\.com|outlook\.com|aol\.com|hotmail\.com)$/i;
+
+    // Se o Regex for verdadeiro no teste na variável email E se o teste na varável email não tiver (!) os valores da variável dominios proibidos
+    let Valido = FormatoValido.test(email) && !DominiosProibidos.test(email) && !FormatoInvalido.test(email);
+
+    if (Valido) {
+
+        console.log(`${email} é um email válido.`);
+
+    } else {
+
+        console.log(`${email} é um email inválido.`);
+
+    }
+
+}
+
+// Teste
+Validacao (`teste@teste.com,br`);
+Validacao (`teste@gmail.com`);
+Validacao (`teste@hotmail.com`);
+Validacao (`teste@teste.com.br`);
